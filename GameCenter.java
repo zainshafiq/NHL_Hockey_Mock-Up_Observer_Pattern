48fc9fa188d046e24c3c8440444dbd9bc5f9619e
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2019-03-21 17:12:06 -0600

    Commit

--- GameCenter.java
+++ GameCenter.java
@@ -4,25 +4,38 @@
 package NHL_Lab;
 
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * @author trevordouglas
  *
  */
 
-
-import java.util.Dictionary;
-import java.util.Hashtable;
-
-
-public class GameCenter {
+public class GameCenter implements GameSubject {
 	
+	Set <GameObserver> gameObservers; 
+	
+	private int homeGameID;
+	private int visitorGameID;
+	private int homeScore;
+	private int visitorScore;
+	private int currentPeriod;
 	private Hashtable<String, Integer> teams;
-	private ArrayList<Game> todaysGames;
 	
-	public GameCenter()
-	{
+	public GameCenter(int homeGameID, int visitorGameID, int homeScore, 
+						int visitorScore, int currentPeriod) {
+		
+		gameObservers = new HashSet<GameObserver>();
+		
+		this.homeGameID = homeGameID;
+		this.visitorGameID = visitorGameID;
+		this.homeScore = homeScore;
+		this.visitorScore = visitorScore;
+		this.currentPeriod = currentPeriod;
+		
 		teams  = new Hashtable<String, Integer>();
 		teams.put("Toronto Maple Leafs", 1);
 		teams.put("Montreal Canadiens", 2);
@@ -30,34 +43,61 @@ public class GameCenter {
 		teams.put("Vancouver Canucks", 4);
 		teams.put("Calgary Flames",  5);
 		teams.put("Ottawa Senators", 6);
-		todaysGames = new ArrayList<Game>();
-		
+		}
+	
+	
+	@Override
+	public void addObserver(GameObserver gameObserver) {
+		gameObservers.add(gameObserver);
 	}
 	
-	public int getTeamID(String teamName)
-	{
-		Integer n = teams.get(teamName);
-		return n.intValue();
-		
+	@Override
+	public void removeObserver(GameObserver gameObserver) {
+		gameObservers.remove(gameObserver);
 	}
-	
-	public void addGame(Game g)
-	{
-		
-		todaysGames.add(g);
-		
-	}
-	
-	public void addTeam(String teamName)
-	{
-		
-		
-		
-	}
-	
-	public ArrayList<Game> getTodaysGames()
-	{
-		return todaysGames;
-	}
-}
 
+	@Override
+	public void doNotify() {
+		Iterator<GameObserver> it = gameObservers.iterator();
+		
+		while (it.hasNext()) {
+			GameObserver gameObserver = it.next();
+			gameObserver.doUpdate(homeGameID, visitorGameID, homeScore, 
+					visitorScore, currentPeriod);
+		}
+	}
+
+	public void setHomeID(int newHomeID) {
+		System.out.println("\nThe Home Team, The Vancouver Canucks '" + newHomeID + "'");
+		homeScore = newHomeID;
+		doNotify();
+	}
+	
+	public void setVisitorID(int newVisitorID) {
+		System.out.println("\nThe Visitor Team, The Calgary Flames '" + newVisitorID + "'");
+		homeScore = newVisitorID;
+		doNotify();
+	}
+	
+	public void setHomeScore(int newHomeScore) {
+		System.out.println("\nThe Home Team Score " + newHomeScore);
+		homeScore = newHomeScore;
+		doNotify();
+	}
+	
+	public void setVisitorScore(int newVisitorScore) {
+		System.out.println("\nThe Visitor Team Score " + newVisitorScore);
+		homeScore = newVisitorScore;
+		doNotify();
+	}
+	
+	public void setCurrentPeriod(int newCurrentPeriod) {
+		System.out.println("\nCurrent Period is " + newCurrentPeriod);
+		homeScore = newCurrentPeriod;
+		doNotify();
+	}
+	
+	
+}
+	
+	
