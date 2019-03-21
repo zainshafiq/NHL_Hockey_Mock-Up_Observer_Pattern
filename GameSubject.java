e2a1d6276c062bb7120af65c4fa2760ee32cc9b1
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2019-03-21 17:15:03 -0600

    Commit

---
+++ GameSubject.java
@@ -0,0 +1,8 @@
+package NHL_Lab;
+
+public interface GameSubject {
+
+	public void addObserver(GameObserver gameObserver);
+	public void removeObserver(GameObserver gameObserver);
+	public void doNotify();
+}
