d1964a7117f860bab656d05d3fddfb09d44b933b
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2019-03-21 17:18:17 -0600

    Commit

--- GameObserver.java
+++ GameObserver.java
@@ -5,6 +5,5 @@ package NHL_Lab;
 
 public interface GameObserver {
 
-	public void doUpdate(int homeGameID, int visitorGameID, int homeScore, 
-			    			int visitorScore, int currentPeriod);
+	public void doUpdate(int homeGameID, int visitorGameID, int homeScore, int visitorScore, int currentPeriod);
 }
