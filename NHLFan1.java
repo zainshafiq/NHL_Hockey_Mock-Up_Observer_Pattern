483da1408117422a52f6a131d37350de4ea7dcb0
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2019-03-24 22:03:46 -0600

    Commit

--- NHLFan1.java
+++ NHLFan1.java
@@ -1,9 +1,10 @@
-//--------- NHLFan1.java -------------
-
 package NHL_Lab;
 
 public class NHLFan1 implements GameObserver {
 
-	//.....
-	
+	@Override
+	public void doUpdate(int homeGameID, int visitorGameID, int homeScore, int visitorScore, int currentPeriod) {
+		// TODO Auto-generated method stub
+		
+	}
 }
