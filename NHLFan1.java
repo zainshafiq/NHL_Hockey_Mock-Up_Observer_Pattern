cd66fd49053a2b3c95a5d3789691b0bc4bcf8669
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2019-03-24 22:04:39 -0600

    Commit

--- NHLFan1.java
+++ NHLFan1.java
@@ -5,6 +5,7 @@ public class NHLFan1 implements GameObserver {
 	@Override
 	public void doUpdate(int homeGameID, int visitorGameID, int homeScore, int visitorScore, int currentPeriod) {
 		// TODO Auto-generated method stub
-		
-	}
+				System.out.println("NHL Fan 1 just found out the home score " 
+				         + homeScore + " and the visitor score " + visitorScore + " in " + currentPeriod + "th period.");
+			}
 }
